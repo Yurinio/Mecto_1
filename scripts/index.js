6da15335b27e04dfd3cd84cc7a8117dfa23b2354
Yuri Kondratuyk <yurinion00013@gmail.com>
2022-06-22 15:55:45 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,34 @@
 const editbutton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const ClosePopupButton = document.querySelector('.popup__close-img');
-const popupForm = document.querySelector('.popup__form');
+const сlosePopupButton = document.querySelector('.popup__close-img');
+const formElement = document.querySelector('.popup__form');
 
 editbutton.addEventListener('click', openPopup)
-ClosePopupButton.addEventListener('click', ClosePopup)
+сlosePopupButton.addEventListener('click', сlosePopup)
 
 function openPopup(event) {
     event.preventDefault()
     popup.classList.add('popup__opened')
-
-    // Set title and description
-    const title = document.getElementById('title').innerHTML
-    const description = document.querySelector('.profile__subtitle').innerHTML
-
-    document.getElementById('headingTitle').value = title
-    document.getElementById('headingDecription').value = description
+    const nameInput = document.getElementById('title').textContent
+    const jobInput = document.querySelector('.profile__subtitle').textContent
+    document.getElementById('name').value = nameInput
+    document.getElementById('job').value = jobInput
 }
-function ClosePopup() {
+function сlosePopup() {
     popup.classList.remove('popup__opened')
 }
-
-function saveData(event) {
+function formSubmitHandler(event) {
     event.preventDefault()
-    const title = document.getElementById('headingTitle').value
-    const description = document.getElementById('headingDecription').value
-
-    document.getElementById('title').innerHTML = title
-    document.querySelector('.profile__subtitle').innerHTML = description
-    ClosePopup()
+    const nameInput = document.getElementById('name').value
+    const jobInput = document.getElementById('job').value
+    document.getElementById('title').textContent = nameInput
+    document.querySelector('.profile__subtitle').textContent = jobInput
+    сlosePopup()
 }
-
 popup.addEventListener('click', (event) => {
     if (event.target.className === 'popup__container') {
-        ClosePopup()
+        сlosePopup()
     }
 }, false)
 
-popupForm.addEventListener('submit', saveData)
+formElement.addEventListener('submit', formSubmitHandler); 
